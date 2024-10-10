904e86af82ef3a6bb5fe8ca0bd34c00879631978
Laura <laura.zvirbule90@gmail.com>
2024-10-10 21:18:24 +0100

    Update package.json with correct homepage for deployment

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,6 @@
 import project1_img from "../assets/juicy-recipes.png";
 import project2_img from "../assets/fitme.png";
 import project3_img from "../assets/trattoria-restaurant.png";
-import project4_img from "../assets/project_4.svg";
-import project5_img from "../assets/project_5.svg";
-import project6_img from "../assets/project_6.svg";
 
 const mywork_data = [
   {
